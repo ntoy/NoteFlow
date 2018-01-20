b1108d9d24260b0a1a128f5675880f2f7bc943aa
Nico Toy <ntoy@princeton.edu>
2018-01-20 18:03:35 -0500

    Finished renaming voice to part hopefully??

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -8,7 +8,7 @@ public class Note {
 
     private int part;
 
-    public Note(Pitch pitch, MusicXMLAbsTime onsetTime, MusicXMLDur duration, int voice) {
+    public Note(Pitch pitch, MusicXMLAbsTime onsetTime, MusicXMLDur duration, int part) {
         this.pitch = pitch;
         this.onsetTime = onsetTime;
         this.duration = duration;
