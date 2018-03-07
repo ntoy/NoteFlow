2443a74e033f920f962dbae538de05603e3e682d
Nico Toy <ntoy@princeton.edu>
2018-03-06 21:46:54 -0500

    Added Time sig field to Note

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -5,12 +5,15 @@ public class Note {
     private Pitch pitch;
     private AbsoluteTime onsetTime;
     private Duration duration;
+    private TimeSig timeSig;
     private int voice;
 
-    public Note(Pitch pitch, AbsoluteTime onsetTime, Duration duration, int voice) {
+    public Note(Pitch pitch, AbsoluteTime onsetTime, Duration duration,
+                TimeSig timeSig, int voice) {
         this.pitch = pitch;
         this.onsetTime = onsetTime;
         this.duration = duration;
+        this.timeSig = timeSig;
         this.voice = voice;
     }
 
@@ -28,6 +31,10 @@ public class Note {
 
     public int getVoice() {
         return voice;
+    }
+
+    public TimeSig getTimeSig() {
+        return timeSig;
     }
 
     @Override
