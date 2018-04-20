5e1e32688782f5726a0b1f8ff21909ccca4d0319
Nico Toy <ntoy@princeton.edu>
2018-04-20 14:08:58 -0400

    More constants

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,8 +1,9 @@
 package main.java;
 
 public class Constants {
-    public static final int MAX_VOICES = 8;
+    static final int MAX_VOICES = 8;
     static final int BASIS_LENGTH = 5;
+    static final int VECTOR_LENGTH = 14;
 
     private Constants() {}
 }
